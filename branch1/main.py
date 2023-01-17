b8bbe5ddef39a46d6a60271b91946de48627cb97
Ralleds <ralleds@gmail.com>
2023-01-17 11:03:52 +0500

    Initial Commit

---
+++ branch1/main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def calculation(a, b):
+
+    for i in range(a):
+        for j in range(b):
+            print(i+j, end=' ')
+        print()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    a, b = int(input()), int(input())
+    calculation(a, b)
+

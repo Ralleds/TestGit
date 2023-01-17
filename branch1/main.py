030730ab88de49d04bcc25848995117ae7b850b5
Ralleds <ralleds@gmail.com>
2023-01-17 11:10:54 +0500

    Added print to explain matrix

--- branch1/main.py
+++ branch1/main.py
@@ -14,7 +14,7 @@ def calculation(a, b):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print("Choose the lenght of matrix")
+    print("Choose the lenght and height of matrix")
     a, b = int(input()), int(input())
     calculation(a, b)
 
